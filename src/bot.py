42336c10ad77fad89255f190a0e19e33c1fdc5ba
Michael Cetrulo <mail2samus@gmail.com>
2014-05-22 07:55:14 -0300

    newer_than_id as cli argument

--- src/bot.py
+++ src/bot.py
@@ -8,12 +8,14 @@ from utils import clean_settings
 
 
 if __name__ == '__main__':
+    import sys
     from datetime import datetime
 
     settings = clean_settings(raw_settings)
     bot = Reddit(user_agent=settings['user_agent'], cache_timeout=0)
 
-    submissions = submissions_gen(bot, settings)
+    newer_than_id = sys.argv[1] if len(sys.argv) > 1 else None
+    submissions = submissions_gen(bot, settings, newer_than_id)
     submissions = (submission for submission in submissions if filter_submission(submission, settings))
 
     for submission in submissions:
