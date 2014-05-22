275992f2e62e61dd1a1d28f6b3a87a8d7ae619cd
Michael Cetrulo <mail2samus@gmail.com>
2014-05-22 06:12:27 -0300

    submissions crawler

--- src/bot.py
+++ src/bot.py
@@ -1,8 +1,50 @@
+#!/usr/bin/env python
 from praw import Reddit
 
 import settings as raw_settings
 from utils import clean_settings
 
-settings = clean_settings(raw_settings)
 
-bot = Reddit(user_agent=settings['user_agent'])
+def submissions_gen(bot, settings, newer_than_id=None):
+    # construct multireddit url to query from list of target subreddits
+    target_subreddit = bot.get_subreddit('+'.join(settings['target_subreddits']))
+
+    # if there's no anchor get the current newest and continue from there
+    if newer_than_id is None:
+        submissions = target_subreddit.get_new(limit=1)
+
+        try:
+            newer_than_id = next(submissions).name
+        except StopIteration:
+            # if it's an empty subreddit set as None
+            # brings everything that gets posted from here until the next request
+            newer_than_id = None
+
+    next_anchor_id = None
+
+    while True:
+        # uses "before" API param because /new lists from newest to oldest
+        # limit=None brings everything that matches (even if it takes multiple requests)
+        submissions = target_subreddit.get_new(params={'before': newer_than_id}, limit=None)
+
+        for submission in submissions:
+            yield submission
+
+            # save the first id as anchor since it'll be the newest
+            if next_anchor_id is None:
+                next_anchor_id = submission.name
+
+        # avoid getting newer_than_id=None when there's no results from the previous iterations
+        if next_anchor_id is not None:
+            newer_than_id, next_anchor_id = next_anchor_id, None
+
+
+if __name__ == '__main__':
+    from datetime import datetime
+
+    settings = clean_settings(raw_settings)
+
+    bot = Reddit(user_agent=settings['user_agent'], cache_timeout=0)
+
+    for submission in submissions_gen(bot, settings):
+        print('[{}] {} - {}'.format(datetime.now().isoformat(), submission.created_utc, submission))
